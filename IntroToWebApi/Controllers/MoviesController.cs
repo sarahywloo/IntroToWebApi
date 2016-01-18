02674bf875e31fbc1af5b905f9a71cdeeb03806f
Sarah Kim <sarahywloo@gmail.com>
2016-01-18 16:19:35 -0600

    Intro to Web API

---
+++ IntroToWebApi/Controllers/MoviesController.cs
@@ -0,0 +1,51 @@
+﻿using IntroToWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroToWebApi.Controllers
+{
+    public class MoviesController : ApiController {
+
+        //Static properties exist on the class itself, will exist across controllers for now, NOT for actual production use.
+        public static IList<Movie> _movies = new List<Movie>() {
+            new Movie() {
+                Id = 0,
+                Title = "Star Trek"
+            }
+        };
+
+        //GET: /api/movies/5
+        public Movie Get(int id) {
+            return _movies.First(x => x.Id == id);
+            //return _movies[id];
+            //return new Movie() {
+            //    Id = id,
+            //    Title = "Star Wars"
+            //};
+        }
+
+        //POST:
+        //returns a status quo instead of void
+        public IHttpActionResult Post(Movie newMovie) {
+            _movies.Add(newMovie);
+            return Ok(); 
+        }
+
+        public IHttpActionResult Post(MovieBattle battle) {
+            return Ok();
+        }
+
+        public IHttpActionResult Post(int id, Movie changes) {
+            var dbMovie = _movies.First(x => x.Id == id);
+            dbMovie.Id = changes.Id;
+            dbMovie.Title = changes.Title;
+            dbMovie.Genre = changes.Genre;
+            dbMovie.ReleaseDate = changes.ReleaseDate;
+            return Ok();
+        }
+    }
+}
---
+++ IntroToWebApi/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroToWebApi.Models {
+    public class Movie {
+
+        //every model should have an Id! to be used in databases.
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        //Adding a list of type Actor into our movie class
+        public List<Actor> Actors { get; set; }
+    }
+}
